5f924b86841c536665db7f9373d03219e0242ae9
lechatn <noe.lechat@ynov.com>
2024-04-25 08:56:45 +0200

    Init game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InitGame initialize = new InitGame();
+        PlayGame game = new PlayGame(initialize.getWordToFind(), initialize.getnumberOfTries());
     }
 }
