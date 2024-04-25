b631c54d849afed71153f880195c0eae356c3d9f
EdgarCou <edgar.couthouis@ynov.com>
2024-04-25 20:49:59 +0200

    commit before merge

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Library library = new Library();
         InitGame initialize = new InitGame(library.words);
-       PlayGame game = new PlayGame(initialize.getWordToFind(), initialize.getnumberOfTries());
+        PlayGame game = new PlayGame(initialize.getWordToFind(), initialize.getnumberOfTries());
         System.out.println("The word to find is: " + initialize.getWordToFind());
     }
 }
