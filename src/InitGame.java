d953f4c88eb3d986b66f416501a7025fef5eb6ae
lechatn <noe.lechat@ynov.com>
2024-04-25 10:49:58 +0200

    Finish to initialize the game

--- src/InitGame.java
+++ src/InitGame.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class InitGame {
 
     private Integer numberOfTries;
-    private String numberOfLetters;
+    private Integer numberOfLetters;
     private String wordToFind;
     private List<String> library;
 
@@ -20,25 +20,53 @@ public class InitGame {
 
     }
 
-    private String askNumberOfLetters() {
+    private Integer askNumberOfLetters() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many letters should the word contain?");
-        return scanner.nextLine();
+        String numberOfLetters = scanner.nextLine();
+        if(!numberOfLetters.matches("[0-9]+")) {
+            System.out.println("Please enter a valid number");
+            return askNumberOfLetters();
+        }
+        try {
+            if (Integer.valueOf(numberOfLetters) < 2) {
+                throw new IllegalArgumentException("The number of letters should be greater than 0");
+            } else if (Integer.valueOf(numberOfLetters) > 14) {
+                throw new IllegalArgumentException("The number of letters should be less than 14");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return askNumberOfLetters();
+        }
+        return Integer.valueOf(numberOfLetters);
     }
 
     private Integer askNumberOfTries() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many tries do you want?");
-        return scanner.nextInt();
+        System.out.println("How many tries do you want ?");
+        String numberOfTries = scanner.nextLine();
+        if(!numberOfTries.matches("[0-9]+")) {
+            System.out.println("Please enter a valid number");
+            return askNumberOfTries();
+        }
+        try {
+            if (Integer.valueOf(numberOfTries) < 1) {
+                throw new IllegalArgumentException("The number of tries should be greater than 1");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return askNumberOfTries();
+        }
+        return Integer.valueOf(numberOfTries);
     }
 
-    private String generateWord(String numberOfLetters) {
+    private String generateWord(Integer numberOfLetters) {
 
         String word = "";
 
         do {
             word = library.get((int) (Math.random() * library.size()));
-        } while (word.length() != Integer.parseInt(numberOfLetters)); {}
+        } while (word.length() != numberOfLetters); {}
         return word;
     }
 
