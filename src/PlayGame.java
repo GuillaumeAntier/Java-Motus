02cbfb55402d7365059f7d4d10322b800df1e9e3
Louka-Gennies <146700433+Louka-Gennies@users.noreply.github.com>
2024-04-26 07:50:27 +0200

    make a timer, change condition of victory in playgame, add loose message

--- src/PlayGame.java
+++ src/PlayGame.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 
 public class PlayGame {
-    private String wordToFind;
+    private final String wordToFind;
     private Integer numberOfTries;
+    private Integer timeLeft;
 
     public PlayGame(String wordToFind, Integer numberOfTries) {
         this.wordToFind = wordToFind;
         this.numberOfTries = numberOfTries;
+        this.timeLeft = 300;
+        Timer timer = new Timer();
     }
 
     public void startGame(){
+        startTimer();
         boolean findWord = false;
-        while (numberOfTries != 0 && !findWord ) {
+        while (numberOfTries > 0 && !findWord && timeLeft > 0) {
+            int minutes = timeLeft / 60;
+            int seconds = timeLeft % 60;
+            System.out.print("\rTime left: " + minutes + " minutes " + seconds + " seconds\n");
             String word = askUser();
-            if (word.equals(wordToFind)){
+            if (word.equalsIgnoreCase(wordToFind)){
                 displayGame(word);
                 System.out.println("Congratulations, you found the word!");
                 findWord = true;
+                break;
             } else {
                 numberOfTries--;
                 displayGame(word);
@@ -25,15 +38,18 @@ public class PlayGame {
         if (numberOfTries == 0){
             System.out.println("You lost, the word to find was: " + wordToFind);
         }
+        if (timeLeft <= 0) {
+            System.out.println("Time's up! The word to find was: " + wordToFind);
+        }
     }
 
     public void displayGame(String word){
-        System.out.println("You put the following word: " + word);
+        System.out.println("You put the following word: " + word.toLowerCase());
         System.out.println(generateLine(wordToFind.length()));
         for (int i = 0; i < wordToFind.length(); i++){
-            if (word.charAt(i) == wordToFind.charAt(i)){
+            if (word.toLowerCase().charAt(i) == wordToFind.toLowerCase().charAt(i)){
                 System.out.print("|" +"\033[42;30m" + " " + word.charAt(i) + " " + "\033[0m");
-            } else if ( wordToFind.contains(String.valueOf(word.charAt(i)))){
+            } else if ( wordToFind.toLowerCase().contains(String.valueOf(word.toLowerCase().charAt(i)))){
                 System.out.print("|" +"\033[43;30m" + " " + word.charAt(i) + " " + "\033[0m");
             }
             else {
@@ -61,6 +77,16 @@ public class PlayGame {
             return askUser();
         }
         return word;
+    }
+
+    private void startTimer() {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(() -> {
+            timeLeft--;
+            if (timeLeft <= 0) {
+                executor.shutdown();
+            }
+        }, 0, 1, TimeUnit.SECONDS);
     }
 }
 
